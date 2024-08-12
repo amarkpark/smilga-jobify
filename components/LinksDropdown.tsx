559dfa6aafb7f5e35c365fcc3f341565fac461c0
Amanda Mark <amarkpark@gmail.com>
2024-08-12 17:28:50 -0600

    Lesson 119: set up Links Dropdown Menu

--- components/LinksDropdown.tsx
+++ components/LinksDropdown.tsx
@@ -1,9 +1,34 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { AlignLeft } from 'lucide-react';
+import { Button } from './ui/button';
+import links from '@/utils/links';
+import Link from 'next/link';
 
+// @ToDo: Use the open state of the dropdown to change the icon
 function LinksDropdown() {
   return (
-    <div>
-      <h1 className="text-3xl">LinksDropdown</h1>
-    </div>
+  <DropdownMenu>
+    <DropdownMenuTrigger asChild className="lg:hidden">
+      <Button variant="ghost" size="icon">
+        <AlignLeft />
+        <span className="sr-only">Links</span>
+      </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent className="w-52 lg:hidden" align="start" sideOffset={25}>
+      {links.map((link) => {
+        return <DropdownMenuItem key={link.href}>
+          <Link href={link.href} className="flex items-center gap-x-2">
+            {link.icon} <span className="capitalize">{link.label}</span>
+          </Link>
+        </DropdownMenuItem>
+      })}
+    </DropdownMenuContent>
+  </DropdownMenu>
   )
 }
 
