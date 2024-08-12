3c2a81fb41429c451dde6c67bb6bdb585e3be225
Amanda Mark <amarkpark@gmail.com>
2024-08-12 16:04:48 -0600

    Lesson 118: Set up Navbar, address afterSignOutUrl deprecation

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,16 @@
+import LinksDropdown from "./LinksDropdown";
+import { UserButton } from "@clerk/nextjs";
+import ThemeToggle from "./ThemeToggle";
 
 function Navbar() {
   return (
-    <div>
-      <h1 className="text-3xl">Navbar</h1>
-    </div>
+    <nav className="bg-muted py-4 sm:px-16 lg:px-24 flex items-center justify-between">
+      <LinksDropdown />
+      <div className="flex items-center gap-x-4">
+        <ThemeToggle />
+        <UserButton />
+      </div>
+    </nav>
   )
 }
 
