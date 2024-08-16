3545dbb6b4c5361b1e577aa6cc8361a042452c86
Amanda Mark <amarkpark@gmail.com>
2024-08-16 15:33:21 -0600

    Lesson 122: Add Shadcn Forms and CreateJobForm.tsx component

--- app/(dashboard)/add-job/page.tsx
+++ app/(dashboard)/add-job/page.tsx
@@ -1,9 +1,10 @@
+import CreateJobForm from "@/components/CreateJobForm"
 
-const AddJobPage = () => {
+function AddJobPage() {
   return (
-    <div>
-      <h1 className="text-3xl">AddJobPage</h1>
-    </div>
+    <>
+      <CreateJobForm />
+    </>
   )
 }
 
