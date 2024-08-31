434e8a3958c55a7e6dbd4baa041fc02225b77cef
Amanda Mark <amarkpark@gmail.com>
2024-08-30 22:20:05 -0600

    Lesson 136: Add query param updates to search form functionality

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -13,22 +13,42 @@ import {
 import { JobStatus } from "@/utils/types";
 
 function SearchForm() {
+  const searchParams = useSearchParams();
+  const search = searchParams.get("search") as string || "";
+  const jobStatus = searchParams.get("searchedJobStatus") as JobStatus || "all";
+
+  const router = useRouter();
+  const pathname = usePathname();
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    let params = new URLSearchParams();
+
     const formData = new FormData(event.currentTarget);
     const search = formData.get("search") as string;
     const searchedJobStatus = formData.get("job-status") as string;
-    console.log("submitting search", search, searchedJobStatus);
+    console.log("submitting search:", search, searchedJobStatus);
+    params.set("search", search);
+    params.set("jobStatus", searchedJobStatus);
+    console.log(params.toString());
+
+    router.push(`${pathname}?${params.toString()}`);
   }
+
   return (
     <form 
       className="bg-muted mb-16 p-8 grid sm:grid-cols-2 md:grid-cols-3 gap-4 rounded-lg"
       onSubmit={handleSubmit}
     >
-      <Input type="text" placeholder="Search Jobs" name="search" />
+      <Input
+        type="text"
+        placeholder="Search Jobs"
+        name="search" 
+        defaultValue={search}
+      />
       <Select
         onValueChange={(value) => console.log(value)}
-        defaultValue={JobStatus.Applied}
+        defaultValue={jobStatus}
         name="job-status"
       >
         <SelectTrigger>
