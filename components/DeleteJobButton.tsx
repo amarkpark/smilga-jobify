1f13717a372cf9a9b69c576020d6cc8ce53dec0e
Amanda Mark <amarkpark@gmail.com>
2024-09-04 16:24:16 -0600

    Lesson 139: first pass buildout of JobCard

---
+++ components/DeleteJobButton.tsx
@@ -0,0 +1,10 @@
+
+function DeleteJobButton() {
+  return (
+    <h1 className="text-3xl">
+      DeleteJobButton
+    </h1>
+  )
+}
+
+export default DeleteJobButton
--- components/JobCard.tsx
+++ components/JobCard.tsx
@@ -1,9 +1,37 @@
 import { JobType } from "@/utils/types";
+import { MapPin, Briefcase, CalendarDays, RadioTower } from "lucide-react";
+
+import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
+import { Badge } from "./ui/badge";
+import JobDetail from "./JobDetail";
+import DeleteJobButton from "./DeleteJobButton";
+
 function JobCard({ job }: { job: JobType }) {
   return (
-    <h1 className="text-3xl">
-      JobCard
-    </h1>
+    <Card className="bg-muted">
+      <CardHeader>
+        <CardTitle>{job.position}</CardTitle>
+        <CardDescription>{job.company}</CardDescription>
+      </CardHeader>
+      <Separator />
+      <CardContent></CardContent>
+      <CardFooter className="flex gap-4">
+        <Button asChild size="sm">
+          <Link href={`/jobs/${job.id}`}>Edit</Link>
+        </Button>
+        <DeleteJobButton job={job} />
+      </CardFooter>
+    </Card>
   )
 }
 
